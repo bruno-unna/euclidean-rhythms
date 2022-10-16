29335aa796420395725143c960c42e5127e2ed98
Bruno Unna <bruno.unna@gmail.com>
2022-10-16 10:04:41 +0100

    Add e(3,8) example as a comment in code

--- src/euclidean_algorithms.c
+++ src/euclidean_algorithms.c
@@ -5,6 +5,7 @@ unsigned long e(unsigned short onsets, unsigned short beats) {
     fprintf(stderr,
             "number of onsets (%d) can't be larger than number of beats (%d)",
             onsets, beats);
+            return -1;
   }
   // er(g, r)
   return 0b10010110;
@@ -26,4 +27,10 @@ g=(4,1) r=(2,0)
 g=(2,10) r=(2,1)
 g=(2,101) r=(0,_)
 e=   101101
+
+e(3,8)
+g=(3,1) r=(5,0)
+g=(3,10) r=(2,0)
+g=(2,100) r=(1,10)
+e=   10010010
 */
