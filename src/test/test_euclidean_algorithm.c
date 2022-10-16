567d7461a26821b39ca9169dc227dc37cfd84da6
Bruno Unna <bruno.unna@gmail.com>
2022-10-16 09:42:58 +0100

    Fix the test to actually test something

--- src/test/test_euclidean_algorithm.c
+++ src/test/test_euclidean_algorithm.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "../euclidean_algorithms.h"
 
 int main() {
-   int r = e();
-   if (r == 4) {
-      printf("Received the expected result (4)");
+   unsigned long r = e(3, 8);
+   if (r == 0b10010010) {
+      printf("Received the expected result (x..x..x.)");
    } else {
-      printf("Received a wrong result (%d)", r);
+      printf("Received a wrong result (%8lx)", r);
       return 1;
    }
    return 0;
