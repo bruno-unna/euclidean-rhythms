222b7ce8433c10e128e333dd5d815977dfa5e282
Bruno Unna <bruno.unna@gmail.com>
2022-12-31 17:18:37 +0000

    Improve the format of the C code

--- src/plugins/plugin.c
+++ src/plugins/plugin.c
@@ -32,7 +32,7 @@ typedef struct {
   LV2_URID time_speed;
 } EuclideanURIs;
 
-typedef enum { 
+typedef enum {
   EUCLIDEAN_CONTROL = 0,
   EUCLIDEAN_BEATS = 1,
   EUCLIDEAN_ONSETS = 2,
@@ -44,56 +44,54 @@ typedef enum {
 } PortIndex;
 
 typedef struct {
-  LV2_URID_Map*  map;    // URID map feature
+  LV2_URID_Map *map;     // URID map feature
   LV2_Log_Logger logger; // Logger API
-  EuclideanURIs      uris;   // Cache of mapped URIDs
+  EuclideanURIs uris;    // Cache of mapped URIDs
 
   struct {
-    LV2_Atom_Sequence* control;
-    uint8_t*          beats;
-    uint8_t*          onsets;
-    uint8_t*          rotation;
-    uint8_t*          channel;
-    uint8_t*          note;
-    uint8_t*          velocity;
-    LV2_Atom_Sequence* midiout;
+    LV2_Atom_Sequence *control;
+    uint8_t *beats;
+    uint8_t *onsets;
+    uint8_t *rotation;
+    uint8_t *channel;
+    uint8_t *note;
+    uint8_t *velocity;
+    LV2_Atom_Sequence *midiout;
   } ports;
 
   // Variables to keep track of the tempo information sent by the host
-  double rate;  // Sample rate
-  float  bpm;   // Beats per minute (tempo)
-  float  speed; // Transport speed (usually 0=stop, 1=play)
+  double rate; // Sample rate
+  float bpm;   // Beats per minute (tempo)
+  float speed; // Transport speed (usually 0=stop, 1=play)
 } Euclidean;
 
-static void
-connect_port(LV2_Handle instance, uint32_t port, void* data)
-{
-  Euclidean* self = (Euclidean*)instance;
+static void connect_port(LV2_Handle instance, uint32_t port, void *data) {
+  Euclidean *self = (Euclidean *)instance;
 
   switch (port) {
   case EUCLIDEAN_CONTROL:
-    self->ports.control = (LV2_Atom_Sequence*)data;
+    self->ports.control = (LV2_Atom_Sequence *)data;
     break;
   case EUCLIDEAN_BEATS:
-    self->ports.beats = (uint8_t*)data;
+    self->ports.beats = (uint8_t *)data;
     break;
   case EUCLIDEAN_ONSETS:
-    self->ports.onsets = (uint8_t*)data;
+    self->ports.onsets = (uint8_t *)data;
     break;
   case EUCLIDEAN_ROTATION:
-    self->ports.rotation = (uint8_t*)data;
+    self->ports.rotation = (uint8_t *)data;
     break;
   case EUCLIDEAN_CHANNEL:
-    self->ports.channel = (uint8_t*)data;
+    self->ports.channel = (uint8_t *)data;
     break;
   case EUCLIDEAN_NOTE:
-    self->ports.note = (uint8_t*)data;
+    self->ports.note = (uint8_t *)data;
     break;
   case EUCLIDEAN_VELOCITY:
-    self->ports.velocity = (uint8_t*)data;
+    self->ports.velocity = (uint8_t *)data;
     break;
   case EUCLIDEAN_MIDI_OUT:
-    self->ports.midiout = (LV2_Atom_Sequence*)data;
+    self->ports.midiout = (LV2_Atom_Sequence *)data;
     break;
   default:
     break;
@@ -104,25 +102,19 @@ connect_port(LV2_Handle instance, uint32_t port, void* data)
    The activate() method resets the state completely, so the wave offset is
    zero and the envelope is off.
 */
-static void
-activate(LV2_Handle instance)
-{
-  Euclidean* self = (Euclidean*)instance;
+static void activate(LV2_Handle instance) {
+  Euclidean *self = (Euclidean *)instance;
 }
 
 /**
    This plugin does a bit more work in instantiate() than the previous
    examples.  The tempo updates from the host contain several URIs, so those
-   are mapped, and the sine wave to be played needs to be generated based on
-   the current sample rate.
+   are mapped.
 */
-static LV2_Handle
-instantiate(const LV2_Descriptor*     descriptor,
-            double                    rate,
-            const char*               path,
-            const LV2_Feature* const* features)
-{
-  Euclidean* self = (Euclidean*)calloc(1, sizeof(Euclidean));
+static LV2_Handle instantiate(const LV2_Descriptor *descriptor, double rate,
+                              const char *path,
+                              const LV2_Feature *const *features) {
+  Euclidean *self = (Euclidean *)calloc(1, sizeof(Euclidean));
   if (!self) {
     return NULL;
   }
@@ -144,45 +136,39 @@ instantiate(const LV2_Descriptor*     descriptor,
   }
 
   // Map URIS
-  EuclideanURIs* const    uris  = &self->uris;
-  LV2_URID_Map* const map   = self->map;
-  uris->atom_Blank          = map->map(map->handle, LV2_ATOM__Blank);
-  uris->atom_Float          = map->map(map->handle, LV2_ATOM__Float);
-  uris->atom_Object         = map->map(map->handle, LV2_ATOM__Object);
-  uris->atom_Path           = map->map(map->handle, LV2_ATOM__Path);
-  uris->atom_Resource       = map->map(map->handle, LV2_ATOM__Resource);
-  uris->atom_Sequence       = map->map(map->handle, LV2_ATOM__Sequence);
-  uris->time_Position       = map->map(map->handle, LV2_TIME__Position);
-  uris->time_barBeat        = map->map(map->handle, LV2_TIME__barBeat);
+  EuclideanURIs *const uris = &self->uris;
+  LV2_URID_Map *const map = self->map;
+  uris->atom_Blank = map->map(map->handle, LV2_ATOM__Blank);
+  uris->atom_Float = map->map(map->handle, LV2_ATOM__Float);
+  uris->atom_Object = map->map(map->handle, LV2_ATOM__Object);
+  uris->atom_Path = map->map(map->handle, LV2_ATOM__Path);
+  uris->atom_Resource = map->map(map->handle, LV2_ATOM__Resource);
+  uris->atom_Sequence = map->map(map->handle, LV2_ATOM__Sequence);
+  uris->time_Position = map->map(map->handle, LV2_TIME__Position);
+  uris->time_barBeat = map->map(map->handle, LV2_TIME__barBeat);
   uris->time_beatsPerMinute = map->map(map->handle, LV2_TIME__beatsPerMinute);
-  uris->time_speed          = map->map(map->handle, LV2_TIME__speed);
+  uris->time_speed = map->map(map->handle, LV2_TIME__speed);
 
   // Initialise instance fields
-  self->rate       = rate;
-  self->bpm        = 120.0f;
+  self->rate = rate;
+  self->bpm = 120.0f;
 
   return (LV2_Handle)self;
 }
 
-static void
-cleanup(LV2_Handle instance)
-{
-  free(instance);
-}
+static void cleanup(LV2_Handle instance) { free(instance); }
 
 /**
    Update the current position based on a host message.  This is called by
    run() when a time:Position is received.
 */
-static void
-update_position(Euclidean* self, const LV2_Atom_Object* obj)
-{
-  const EuclideanURIs* uris = &self->uris;
+static void update_position(Euclidean *self, const LV2_Atom_Object *obj) {
+  const EuclideanURIs *uris = &self->uris;
 
   // Received new transport position/speed
-  LV2_Atom* beat  = NULL;
-  LV2_Atom* bpm   = NULL;
-  LV2_Atom* speed = NULL;
+  LV2_Atom *beat = NULL;
+  LV2_Atom *bpm = NULL;
+  LV2_Atom *speed = NULL;
   // clang-format off
   lv2_atom_object_get(obj,
                       uris->time_barBeat, &beat,
@@ -193,43 +179,45 @@ update_position(Euclidean* self, const LV2_Atom_Object* obj)
 
   if (bpm && bpm->type == uris->atom_Float) {
     // Tempo changed, update BPM
-    self->bpm = ((LV2_Atom_Float*)bpm)->body;
+    self->bpm = ((LV2_Atom_Float *)bpm)->body;
+    lv2_log_note(&self->logger, "setting bpm to %f\n", self->bpm);
   }
   if (speed && speed->type == uris->atom_Float) {
     // Speed changed, e.g. 0 (stop) to 1 (play)
-    self->speed = ((LV2_Atom_Float*)speed)->body;
+    self->speed = ((LV2_Atom_Float *)speed)->body;
+    lv2_log_note(&self->logger, "setting speed to %f\n", self->speed);
   }
   if (beat && beat->type == uris->atom_Float) {
     // Received a beat position, synchronise
     // This hard sync may cause clicks, a real plugin would be more graceful
     const float frames_per_beat = (float)(60.0 / self->bpm * self->rate);
-    const float bar_beats       = ((LV2_Atom_Float*)beat)->body;
-    const float beat_beats      = bar_beats - floorf(bar_beats);
+    const float bar_beats = ((LV2_Atom_Float *)beat)->body;
+    const float beat_beats = bar_beats - floorf(bar_beats);
+    lv2_log_note(&self->logger, "setting frames/beat to %f\n", frames_per_beat);
+    lv2_log_note(&self->logger, "setting bar beats to %f\n", bar_beats);
+    lv2_log_note(&self->logger, "setting beat beats to %f\n", beat_beats);
   }
 }
 
-static void
-run(LV2_Handle instance, uint32_t sample_count)
-{
-  Euclidean*           self = (Euclidean*)instance;
-  const EuclideanURIs* uris = &self->uris;
+static void run(LV2_Handle instance, uint32_t sample_count) {
+  Euclidean *self = (Euclidean *)instance;
+  const EuclideanURIs *uris = &self->uris;
 
   // Work forwards in time frame by frame, handling events as we go
-  const LV2_Atom_Sequence* in     = self->ports.control;
-  uint32_t                 last_t = 0;
-  for (const LV2_Atom_Event* ev = lv2_atom_sequence_begin(&in->body);
+  const LV2_Atom_Sequence *in = self->ports.control;
+  uint32_t last_t = 0;
+  for (const LV2_Atom_Event *ev = lv2_atom_sequence_begin(&in->body);
        !lv2_atom_sequence_is_end(&in->body, in->atom.size, ev);
        ev = lv2_atom_sequence_next(ev)) {
     // Play the click for the time slice from last_t until now
     // play(self, last_t, (uint32_t)ev->time.frames);
-    // TODO instead of play(), generate a MIDI event
-    lv2_log_note(&self->logger, "Beat!\n");
+    // TODO instead of play(), perhaps generate a MIDI event
 
     // Check if this event is an Object
     // (or deprecated Blank to tolerate old hosts)
     if (ev->body.type == uris->atom_Object ||
         ev->body.type == uris->atom_Blank) {
-      const LV2_Atom_Object* obj = (const LV2_Atom_Object*)&ev->body;
+      const LV2_Atom_Object *obj = (const LV2_Atom_Object *)&ev->body;
       if (obj->body.otype == uris->time_Position) {
         // Received position information, update
         update_position(self, obj);
@@ -246,19 +234,17 @@ run(LV2_Handle instance, uint32_t sample_count)
 }
 
 static const LV2_Descriptor descriptor = {
-  EG_EUCLIDEAN_URI,
-  instantiate,
-  connect_port,
-  activate,
-  run,
-  NULL, // deactivate,
-  cleanup,
-  NULL, // extension_data
+    EG_EUCLIDEAN_URI,
+    instantiate,
+    connect_port,
+    activate,
+    run,
+    NULL, // deactivate,
+    cleanup,
+    NULL, // extension_data
 };
 
 LV2_SYMBOL_EXPORT
-const LV2_Descriptor*
-lv2_descriptor(uint32_t index)
-{
+const LV2_Descriptor *lv2_descriptor(uint32_t index) {
   return index == 0 ? &descriptor : NULL;
 }
