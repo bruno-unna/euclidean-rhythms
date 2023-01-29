fe55575967736be5c5f5c825ed72c167b98cafd2
Bruno Unna <bruno.unna@gmail.com>
2023-01-29 20:04:28 +0000

    Format code

--- src/euclidean.c
+++ src/euclidean.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 typedef struct {
-  unsigned short n; // how many repetitions?
-  unsigned long v;  // of what sequence?
-  unsigned short s; // how long is the sequence?
+    unsigned short n; // how many repetitions?
+    unsigned long v;  // of what sequence?
+    unsigned short s; // how long is the sequence?
 } repeats;
 
 void er(repeats *g, repeats *r) {
-  if (r->n <= 1)
-    return;
-  if (g->n <= r->n) {
-    // distribute some elements of `r` amongst the elements of `g`
-    r->n -= g->n;
-    g->s += r->s;
-    g->v <<= r->s;
-    g->v |= r->v;
-  } else {
-    // forget `r` and split old `g` unto new `g` and new `r`
-    repeats rt = *r;
-    r->v = g->v;
-    r->n = g->n - r->n;
-    r->s = g->s;
-    g->v <<= rt.s;
-    g->v |= rt.v;
-    g->s += rt.s;
-    g->n = rt.n;
-  }
-  er(g, r);
+    if (r->n <= 1)
+        return;
+    if (g->n <= r->n) {
+        // distribute some elements of `r` amongst the elements of `g`
+        r->n -= g->n;
+        g->s += r->s;
+        g->v <<= r->s;
+        g->v |= r->v;
+    } else {
+        // forget `r` and split old `g` unto new `g` and new `r`
+        repeats rt = *r;
+        r->v = g->v;
+        r->n = g->n - r->n;
+        r->s = g->s;
+        g->v <<= rt.s;
+        g->v |= rt.v;
+        g->s += rt.s;
+        g->n = rt.n;
+    }
+    er(g, r);
 }
 
 unsigned long e(unsigned short onsets, unsigned short beats, short rotation) {
@@ -40,25 +40,25 @@ unsigned long e(unsigned short onsets, unsigned short beats, short rotation) {
     } else if (onsets > beats) {
         fprintf(stderr,
                 "number of onsets (%d) can't be larger than number of beats (%d)\n",
-            onsets, beats);
-    for (short i = 0; i < beats; ++i) {
-      result <<= 1;
-      result |= 1;
-    }
-  } else {
-    repeats g = {onsets, 0b1, 1};
-    repeats r = {beats - onsets, 0b0, 1};
-    er(&g, &r);
+                onsets, beats);
+        for (short i = 0; i < beats; ++i) {
+            result <<= 1;
+            result |= 1;
+        }
+    } else {
+        repeats g = {onsets, 0b1, 1};
+        repeats r = {beats - onsets, 0b0, 1};
+        er(&g, &r);
 
-    result = g.v;
-    for (unsigned short i = 1; i < g.n; ++i) {
-      result <<= g.s;
-      result |= g.v;
+        result = g.v;
+        for (unsigned short i = 1; i < g.n; ++i) {
+            result <<= g.s;
+            result |= g.v;
+        }
+        if (r.n > 0) {
+            result <<= r.s;
+            result |= r.v;
+        }
     }
-    if (r.n > 0) {
-      result <<= r.s;
-      result |= r.v;
-    }
-  }
-  return result;
+    return result;
 }
--- test/test_euclidean_algorithm.c
+++ test/test_euclidean_algorithm.c
@@ -2,71 +2,71 @@
 #include "../include/euclidean.h"
 
 int main() {
-   unsigned long r;
+    unsigned long r;
 
     r = e(0, 4, 0);
-   if (r == 0b0000) {
-      printf("Received the expected result (....)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x00L);
-      return 1;
-   }
+    if (r == 0b0000) {
+        printf("Received the expected result (....)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x00L);
+        return 1;
+    }
 
     r = e(3, 8, 0);
-   if (r == 0b10010010) {
-      printf("Received the expected result (x..x..x.)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x52L);
-      return 1;
-   }
+    if (r == 0b10010010) {
+        printf("Received the expected result (x..x..x.)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x52L);
+        return 1;
+    }
 
     r = e(4, 6, 0);
-   if (r == 0b101101) {
-      printf("Received the expected result (x.xx.x)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x2dL);
-      return 1;
-   }
+    if (r == 0b101101) {
+        printf("Received the expected result (x.xx.x)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x2dL);
+        return 1;
+    }
 
     r = e(7, 8, 0);
-   if (r == 0b11111110) {
-      printf("Received the expected result (xxxxxxx.)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0xfeL);
-      return 1;
-   }
+    if (r == 0b11111110) {
+        printf("Received the expected result (xxxxxxx.)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0xfeL);
+        return 1;
+    }
 
     r = e(4, 16, 0);
-   if (r == 0b1000100010001000) {
-      printf("Received the expected result (x...x...x...x...)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x8888L);
-      return 1;
-   }
+    if (r == 0b1000100010001000) {
+        printf("Received the expected result (x...x...x...x...)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x8888L);
+        return 1;
+    }
 
     r = e(1, 4, 0);
-   if (r == 0b1000) {
-      printf("Received the expected result (x...)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x8L);
-      return 1;
-   }
+    if (r == 0b1000) {
+        printf("Received the expected result (x...)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x8L);
+        return 1;
+    }
 
     r = e(10, 8, 0);
-   if (r == 0b11111111) {
-      printf("Received the expected result (xxxxxxxx)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0xffL);
-      return 1;
-   }
+    if (r == 0b11111111) {
+        printf("Received the expected result (xxxxxxxx)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0xffL);
+        return 1;
+    }
 
     r = e(4, 7, 0);
-   if (r == 0b1010101) {
-      printf("Received the expected result (x.x.x.x)\n");
-   } else {
-      printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x33L);
-      return 1;
-   }
+    if (r == 0b1010101) {
+        printf("Received the expected result (x.x.x.x)\n");
+    } else {
+        printf("Received a wrong result (0x%lx), expecting 0x%lx\n", r, 0x33L);
+        return 1;
+    }
 
-   return 0;
+    return 0;
 }
