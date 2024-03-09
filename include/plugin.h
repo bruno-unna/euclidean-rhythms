2c30391d82bab214f4daa530ebf5905815b5cdab
Bruno Unna <bruno.unna@gmail.com>
2024-03-09 17:11:35 +0000

    Move the Euclidean structure to the .h file

--- include/plugin.h
+++ include/plugin.h
@@ -18,6 +18,14 @@
 #ifndef EUCLIDEAN_PLUGIN_H
 #define EUCLIDEAN_PLUGIN_H
 
+#include <lv2/lv2plug.in/ns/ext/atom/atom.h>
+#include <lv2/lv2plug.in/ns/ext/atom/forge.h>
+#include <lv2/lv2plug.in/ns/ext/log/logger.h>
+#include <lv2/lv2plug.in/ns/lv2core/lv2.h>
+#include <lv2/lv2plug.in/ns/lv2core/lv2_util.h>
+
+#include "lv2_uris.h"
+
 #define EUCLIDEAN_URI "https://github.com/bruno-unna/euclidean-rhythms"
 #define EUCLIDEAN_UI_URI "https://github.com/bruno-unna/euclidean-rhythms#ui"
 
@@ -36,5 +44,44 @@ enum {
     NOTE_IDX = 5,
     VELOCITY_IDX = 6
 };
+
+typedef struct {
+    LV2_URID_Map *map;     // URID map feature
+    LV2_Log_Logger logger; // Logger API
+    Euclidean_URIs uris;    // Cache of mapped URIDs
+
+    struct {
+        const LV2_Atom_Sequence *control;
+        const float *knobs[N_GENERATORS][N_KNOBS];
+        LV2_Atom_Sequence *midi_out;
+    } ports;
+
+    // this state is common to all generators
+    struct {
+        float speed;
+        float beats_per_minute;
+        float beats_per_bar;
+        long current_bar;
+        float frames_per_second;
+    } common_state;
+
+    // this state is particular to each generator
+    struct {
+        unsigned short beats;
+        unsigned short onsets;
+        short rotation;
+        unsigned short size_in_bars;
+
+        unsigned long euclidean;
+
+        long reference_frame;
+        unsigned short note_on_index;
+        unsigned short note_off_index;
+        long *note_on_vector;
+        long *note_off_vector;
+
+        unsigned short playing;
+    } state[N_GENERATORS];
+} Euclidean;
 
 #endif //EUCLIDEAN_PLUGIN_H
